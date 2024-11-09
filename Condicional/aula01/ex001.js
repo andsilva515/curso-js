cb36819786a3594197b54901b7ed9191bd6b90cf
andsilva515 <anderson.ssilva@outlook.com>
2024-11-09 17:36:07 -0300

    estrutura condicional simples

---
+++ Condicional/aula01/ex001.js
@@ -0,0 +1,6 @@
+var velocidade = 78.2
+console.log(`A velocidade do seu carro é de ${velocidade}km/h`)
+if (velocidade > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre utilizando cinto de segurança!`)
