2fbf128e5813b93da598c2cc76a5ff84c070ff4b
andsilva515 <anderson.ssilva@outlook.com>
2024-11-09 17:49:31 -0300

    estrutura condicional composta

---
+++ Condicional/aula01/ex002.js
@@ -0,0 +1,19 @@
+/*
+// Qualquer situalção igual a:
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
+*/
+
+// Qualquer situalção diferente de:
+var pais = 'França'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
