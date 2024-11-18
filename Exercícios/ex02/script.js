dcabb292c300e489317340347b7149211874e626
andsilva515 <anderson.ssilva@outlook.com>
2024-11-17 23:05:33 -0300

    exercicio bom dia, boa tarde, boa noite

---
+++ Exercícios/ex02/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    // var hora = data.getHours()
+
+    var hora = 18
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        // Bom Dia!!
+        img.src = 'img/fotomanha.png'
+    } else if (hora >= 12 && hora < 18) {
+        // Boa Tarde!
+        img.src = 'img/fototarde.png'
+    } else {
+        // Boa Noite!
+        img.src = 'img/fotonoite.png'
+    }
+}
+
