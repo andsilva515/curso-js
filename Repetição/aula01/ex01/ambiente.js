0629b4977090bb82850a174cb14e8a5662e555eb
andsilva515 <anderson.ssilva@outlook.com>
2024-11-22 16:02:43 -0300

    exercício estrutura de repetição while

---
+++ Repetição/aula01/ex01/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c ++
+}
+
+
+/*
+var c = 1
+while (c <= 3) {
+    console.log('Tudo bem?')
+    c ++ // c = c + 1
+
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
