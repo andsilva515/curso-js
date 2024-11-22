9593624fffcb4ce3e251b42cc2eadceee218c266
andsilva515 <anderson.ssilva@outlook.com>
2024-11-22 15:57:55 -0300

    estrutura de repetição while

--- Repetição/aula01/ambiente.js
+++ Repetição/aula01/ambiente.js
@@ -1,12 +1,18 @@
 var c = 1
-while (c < 6) {
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c ++
+}
+
+
+/*
+var c = 1
+while (c <= 3) {
     console.log('Tudo bem?')
     c ++ // c = c + 1
 
 }
-
-
-
+*/
 
 
 /*
