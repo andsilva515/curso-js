b0b8aebd9cd3f1f6a9156bb6340565784a6d4589
andsilva515 <anderson.ssilva@outlook.com>
2024-11-22 21:05:23 -0300

    comentário conceito estrutura do while

--- Repetição/aula01/ex02/ambiente.js
+++ Repetição/aula01/ex02/ambiente.js
@@ -1,4 +1,3 @@
-
 /* Estrutura de repetição do while com teste lógico no final */
 
 // Primeiro executa o bloco e depois faz o teste, se o teste for verdadeiro faz o looping e executa o bloco novamente, quando for falso o fluxo termina e sai da estrutura.
