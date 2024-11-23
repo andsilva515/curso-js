097fbbb1756ad4db788718371606a927d39448bf
andsilva515 <anderson.ssilva@outlook.com>
2024-11-22 21:01:52 -0300

    estrutura de repetição do while

--- Repetição/aula01/ex02/ambiente.js
+++ Repetição/aula01/ex02/ambiente.js
@@ -1,3 +1,12 @@
 
+/* Estrutura de repetição do while com teste lógico no final */
+
+// Primeiro executa o bloco e depois faz o teste, se o teste for verdadeiro faz o looping e executa o bloco novamente, quando for falso o fluxo termina e sai da estrutura.
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
 
 
