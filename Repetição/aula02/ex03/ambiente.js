05d05954fe98c788f596f80a1830baeebb995b75
andsilva515 <anderson.ssilva@outlook.com>
2024-11-23 17:40:20 -0300

    comentário conceito estrutura for

--- Repetição/aula02/ex03/ambiente.js
+++ Repetição/aula02/ex03/ambiente.js
@@ -1,3 +1,7 @@
+/* Estrutura de repetição (for) com váriavel de controle */
+
+// São realizdas três passos; Inicialização, teste lógico e incremento
+
 for (c = 1; c <= 10; c++) {
     console.log(c)
 }
