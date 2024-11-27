5341626f83776fdc0fc3d7e85edaab7ff7c54cb4
andsilva515 <anderson.ssilva@outlook.com>
2024-11-27 16:33:25 -0300

    variáveis compostas

---
+++ Variáveis Compostas/aula01/ambiente.js
@@ -0,0 +1,18 @@
+// Tamanho do vetor
+
+let num = [5, 8, 2, 9, 3]
+    num.push(1) // adiciona mais um elemento no vetor.
+    num.sort() // vetor em ordem
+    console.log(`Vetor: [ ${num} ]`)
+    console.log(`O vetor tem ${num.length} posições.`)
+    console.log(`O primeiro valor do vetor é ${num[0]}`)
+    console.log(`O segundo valor do vetor é ${num[1]}`)
+
+
+
+// Leitura de um vetor
+/*
+let num = [5, 8, 2, 9, 3]
+
+console.log(num)
+*/
