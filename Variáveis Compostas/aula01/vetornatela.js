5dce756a9e03d4847012c01451da9f2ccb5d8b77
andsilva515 <anderson.ssilva@outlook.com>
2024-11-27 17:18:38 -0300

    variáveis compostas exercicios

--- Variáveis Compostas/aula01/vetornatela.js
+++
@@ -1,7 +0,0 @@
-let valores = [8, 1, 7, 4, 2, 9]
-    console.log(valores) // forma de mostrar padrão
-
-for(let pos=0; pos < valores.length; pos++) {
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`) // forma de mostrar com formatação
-
-}
