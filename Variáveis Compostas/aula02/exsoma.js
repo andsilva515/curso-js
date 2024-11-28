9c8e991728446fea4cf67d0559a743a667eb34d0
andsilva515 <anderson.ssilva@outlook.com>
2024-11-28 09:59:08 -0300

    realiza a soma de valores

---
+++ Variáveis Compostas/aula02/exsoma.js
@@ -0,0 +1,21 @@
+/*
+function soma(num1, num2) {
+    return num1 + num2
+}
+
+console.log(soma(2, 5))
+*/
+
+/*
+function soma(num1, num2) {
+    return num1 + num2
+}
+
+console.log(soma(2))
+*/
+
+function soma(num1=0, num2=0) {
+    return num1 + num2
+}
+
+console.log(soma(2))
